974a7f58e9f6753cca21d716f85b215d811976c9
Robert Waltham <rob@blockoftext.com>
2023-09-09 12:22:16 -0700

    print location

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
+use wasm_bindgen::prelude::*;
+use web_sys::console;
+
 fn main() {
-    println!("Hello, world!");
+    console::log_1(&"Hello using web-sys".into());
+
+    let js: JsValue = 4.into();
+    console::log_2(&"Logging arbitrary values looks like".into(), &js);
+
+    let document = web_sys::window().unwrap().document().unwrap();
+
+    let loc = document.location().unwrap();
+
+    console::log_1(&loc.into());
 }
